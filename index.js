64c7985991f48ca799fef87c9105f64b650f7761
Kevin Haro <kharo1@student.gsu.edu>
2017-07-13 14:52:22 -0400

    testing to see if no attachments if else is faster

--- index.js
+++ index.js
@@ -96,10 +96,6 @@ app.post('/', function(req, res) {
       const sessionId = findOrCreateSession(sender)
 			const text = event.message.text
       greet(sender)
-  }else if (attachments){
-    //we recieved some sort of attachment
-    sendRequest(sender, {text: "Sorry I can only process text messages for you right now :("})
-    .catch(console.error)
   }
 	}
 	res.sendStatus(200)
