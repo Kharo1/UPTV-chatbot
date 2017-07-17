2fef0cb83115e00e4adfabe4d46fc05c01194084
Kevin Haro <kharo1@student.gsu.edu>
2017-07-17 10:27:07 -0400

    test

--- index.js
+++ index.js
@@ -6,7 +6,6 @@
 var script = document.createElement('script')
 
 script.src = '//code.jquery.com/jquery-1.11.0.min.js'
-document.getElementsByTagName('head')[0].appendChild(script)
 
 const express = require('express'),
  			bodyParser = require('body-parser'),
